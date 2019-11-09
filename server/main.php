464fe7850f42974559e99e498a8306e8630d6e6f
Camilo III P. Ortillo <camortillo@gmail.com>
2019-11-09 15:44:11 -0500

    Fixed indentation

--- server/main.php
+++ server/main.php
@@ -6,36 +6,33 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="codemirror/mode/python/python.js"></script>
-<script>
-</script>
 <style>
-.content {
-  margin: auto;
-}
+  .content {
+    margin: auto;
+  }
 </style>
 <html>
-<body>
+  <body>
+    <div class="content" style="text-align: center">
+      <h1>pyLearner</h1>
+      <button onclick="myFunction()">Try it</button>
+    </div>
 
-<div class="content" style="text-align: center">
-	<h1>pyLearner</h1>
-	<button onclick="myFunction()">Try it</button>
-</div>
+    <script>
+      function myFunction() {
+        var myCodeMirror = CodeMirror(document.body, {
+          value: "def newFunction():\n",
+          mode:  "python",
+          indentUnit: "4"
+        });
+      }
 
-<script>
-function myFunction() {
-	var myCodeMirror = CodeMirror(document.body, {
-	  value: "def newFunction():\n",
-	  mode:  "python",
-	  indentUnit: "4"
-	});
-}
+      myFunction();
+    </script>
 
-myFunction();
-</script>
-
-<?php
-?>
+    <?php
+    ?>
 
 
-</body>
+  </body>
 </html>
