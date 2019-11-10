1cca1693d42f1a55b3aedb7489371015c744b806
Simon Lam <simon@saerie.com>
2019-11-10 03:17:55 -0500

    fix deployment issues

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 var app = express();
 var PORT = 3000;
 
-app.use("/",express.static(path.join(__dirname), "/public"))
+app.use("/",express.static(__dirname + "/public"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use((req, res, next) => {
