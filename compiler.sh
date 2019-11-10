b65737e133afda13aa7e03c756e7e49c2a5036c2
Simon Lam <simon@saerie.com>
2019-11-10 02:06:20 -0500

    COMPILER.SH WORKS
    IT WORKS!!!!

--- compiler.sh
+++ compiler.sh
@@ -3,17 +3,16 @@
 # ensure test.py can run
 chmod 777 test.py
 
-# remove the files if possible
+# create files
 touch output.txt
 touch errors.txt
-rm output.txt
-rm errors.txt
 
 # initialize array for all files within testCases
 testArr=$(ls testCases/*)
 fullArr=${#testArr[@]}
 testArr=$(($fullArr/2))
 fname=$testDiff.txt
+correct=0
 
 # iterating through the array
 for((i=0;i<$halfArr;i++))
@@ -22,4 +21,12 @@ do
 	j=$(($i+$halfArr))
 	# send output to output.txt and errors.txt
 	python3 test.py ${testArr[$i]} >> output.txt 2&>errors.txt
+	truth=$(echo "$(ls -s output)" | head -c1)
+	((correct+=truth))
 done
+
+echo "correct "$correct"/"$halfArr > answers
+
+# remove excess files
+rm output.txt
+rm errors.txt
