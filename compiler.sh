3aef3b21b329093a229cc6d2d5252e865430b458
Simon Lam <simon@saerie.com>
2019-11-10 01:38:10 -0500

    lol
    z

--- compiler.sh
+++ compiler.sh
@@ -9,5 +9,17 @@ touch errors.txt
 rm output.txt
 rm errors.txt
 
-# test compiler
-python3 test.py >> output.txt 2&>errors.txt
+# initialize array for all files within testCases
+testArr=$(ls testCases/*)
+fullArr=${#testArr[@]}
+testArr=$(($fullArr/2))
+fname=$testDiff.txt
+
+# iterating through the array
+for((i=0;i<$halfArr;i++))
+do
+	# for every element
+	j=$(($i+$halfArr))
+	# send output to output.txt and errors.txt
+	python3 test.py ${testArr[$i]} >> output.txt 2&>errors.txt
+done
