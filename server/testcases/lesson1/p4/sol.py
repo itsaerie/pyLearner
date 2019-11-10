6611948f4fbc2d3a4c0b9822c48113564811b1cb
moppr <mantony42@gmail.com>
2019-11-10 09:59:39 -0500

    Add files via upload

---
+++ server/testcases/lesson1/p4/sol.py
@@ -0,0 +1,5 @@
+def absMax(x, y, z):
+    print(max(abs(x), abs(y), abs(z)))
+import sys
+if __name__ == "__main__":
+    eval(sys.argv[1])
---
+++ server/testcases/lesson1/p5/sol.py
@@ -0,0 +1,10 @@
+def compare(x, y):
+	print(x < y)
+	print(x > y)
+	print(x <= y)
+	print(x >= y)
+	print(x == y)
+	print(x != y)
+import sys
+if __name__ == "__main__":
+	eval(sys.argv[1])
