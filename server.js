6ffec5a3e663acb7717aaeccf53bf6db668d8d03
Camilo III P. Ortillo <camortillo@gmail.com>
2019-11-10 01:21:40 -0500

    Attempted fix on post issues

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://104.211.7.172/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
